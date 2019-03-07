356e70255946f24cf89a40723f9cc94db1f77364
Sepulveda <jasepulveda@labs.neit.edu>
2019-03-07 14:49:55 -0500

    Ready For Final

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const io = require('socket.io')(process.env.PORT || 5000);
 const shortid = require('shortid');
 
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+
+const db = require('./config/database');
+
+// Connect To MongoDB Using Mongoose
+mongoose.connect(db.mongoURI, {
+	useNewUrlParser: true
+}).then(function () {
+	console.log("MongoDB Connected");
+}).catch(function(err) {
+	console.log(err);
+});
+
+// Load Users Model
+require('./models/Users');
+var User = mongoose.model('Users');
+
 var players = [];
 
 console.log("Server Running");
 
 io.on('connection', function(socket) {
 	console.log("Connected To Unity");
+
+	socket.emit('connected');
 
 	var thisPlayerID = shortid.generate();
 
@@ -39,6 +63,28 @@ io.on('connection', function(socket) {
 		console.log("Unity Game Says \"Hello\"");
 
 		socket.emit('talkback');
+	});
+
+	socket.on('sendData', function(data) {
+		console.log(JSON.stringify(data));
+		User.findOne({name: data.name}).then(function (user) {
+			console.log(JSON.stringify(user));
+			if (user != null) {
+				socket.emit('registrationFailed', data);
+			} else {
+				var newUser = {
+					name: data.name
+				};
+				new User(newUser).save().then(function(users) {
+					console.log("Sending Data To Database");
+
+					User.find({}).then(function (users) {
+						
+						socket.emit('hideForm', {users});
+					});
+				});
+			}
+		});
 	});
 
 	socket.on('disconnect', function () {
